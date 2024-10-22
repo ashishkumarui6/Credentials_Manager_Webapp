00ce393eb1267149d9caf9d9c943d6210b9eca4d
ashishkumarui6 <ashishkumarui6@gmail.com>
2024-10-22 18:52:49 +0530

    file Add

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+        aspernatur veniam aliquid vel quos reiciendis. Iusto, rerum! Quisquam,
+        laboriosam eaque possimus accusantium autem laborum? Autem tempora
+        architecto ullam harum ipsam dolor rem, laudantium neque molestiae
+        consequuntur eveniet atque voluptatum ducimus amet unde assumenda enim
+        in. Provident perferendis vero neque tempora veritatis, hic nam vitae.
+        Delectus temporibus dolorum eius nemo tempore recusandae reiciendis,
+        blanditiis sapiente quae repellat error quis reprehenderit in,
+        aspernatur corporis tenetur. Dolores consequatur culpa atque adipisci
+        officia itaque, omnis sunt blanditiis qui aliquid assumenda molestias
+        explicabo quas? Accusamus explicabo, accusantium odit optio atque nisi
+        illum quam tempore necessitatibus.
+      </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
